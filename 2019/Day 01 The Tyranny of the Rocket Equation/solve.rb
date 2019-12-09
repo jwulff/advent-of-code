22f12f11da1cba1205ff333611442671f835e662
John Wulff <john@johnwulff.com>
2019-12-09 12:21:51 -0800

    Solve 2019 day 1

--- 2019/Day 01 The Tyranny of the Rocket Equation/solve.rb
+++ 2019/Day 01 The Tyranny of the Rocket Equation/solve.rb
@@ -2,11 +2,30 @@
 
 input = File.read('./INPUT')
 module_masses = input.split.collect &:to_i
-required_fuel = 0
 
-module_masses.each do |mass|
-  required_fuel += mass / 3 - 2
+def fuel_for(mass)
+  mass / 3 - 2
 end
 
-puts required_fuel
+required_masses_fuel = 0
+required_fuel_fuel = 0
 
+module_masses.each do |mass|
+  module_fuel = fuel_for mass
+  fuel_fuel = fuel_for module_fuel
+  added_fuel = fuel_fuel
+  loop do
+    added_fuel = fuel_for added_fuel
+    if added_fuel >= 0
+      fuel_fuel += added_fuel
+    else
+      break
+    end
+  end
+  required_masses_fuel += module_fuel
+  required_fuel_fuel += fuel_fuel
+end
+
+puts "Fuel Required for Module Masses: #{required_masses_fuel}"
+puts "Fuel Required for Fuel: #{required_fuel_fuel}"
+puts "Total Fuel Required: #{required_masses_fuel + required_fuel_fuel}"
