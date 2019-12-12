d0ff1acd88c7d907b5a122a93642108a6159f994
John Wulff <john@johnwulff.com>
2019-12-12 10:10:24 -0800

    Solve 2019 day 4 part 1
    Some real smashing of arrays here. It’s expensive but still plenty fast!

--- 2019/Day 04 Secure Container/solve.rb
+++ 2019/Day 04 Secure Container/solve.rb
@@ -1,3 +1,31 @@
 #!/usr/bin/env ruby
 
 input = (372037..905157)
+
+possibles = 0
+
+def repeating?(x)
+  x = x.to_s
+  x.include?('00') ||
+  x.include?('11') ||
+  x.include?('22') ||
+  x.include?('33') ||
+  x.include?('44') ||
+  x.include?('55') ||
+  x.include?('66') ||
+  x.include?('77') ||
+  x.include?('88') ||
+  x.include?('99')
+end
+
+def increasing?(x)
+  x == x.to_s.split('').collect(&:to_i).sort.collect(&:to_s).join('').to_i
+end
+
+input.each do |candidate|
+  next unless repeating?(candidate)
+  next unless increasing?(candidate)
+  possibles += 1 
+end
+
+puts "Found #{possibles} possible passwords"
