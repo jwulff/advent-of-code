a819320f4716f431ff328b9a1a88c53b23af8448
John Wulff <john@johnwulff.com>
2019-12-12 16:51:10 -0800

    Solve 2019 day 4 part 2
    $ time ./solve.rb
    Found 481 possible passwords for part 1 rules
    Found 299 possible passwords for part 2 rules
    
    real	0m5.572s
    user	0m5.519s
    sys	0m0.035s

--- 2019/Day 04 Secure Container/solve.rb
+++ 2019/Day 04 Secure Container/solve.rb
@@ -2,8 +2,6 @@
 
 input = (372037..905157)
 
-possibles = 0
-
 def repeating?(x)
   !!x.to_s.match(/(\d)\1{1,1}/)
 end
@@ -12,10 +10,25 @@ def increasing?(x)
   x == x.to_s.split('').collect(&:to_i).sort.collect(&:to_s).join('').to_i
 end
 
+def repeating_but_not_too_much?(x)
+  # Remove any sequence of digits repeating more than twice before looking for digits repeating.
+  repeating? x.to_s.gsub(/(\d)\1{2,}/, '')
+end
+
+possibles = 0
 input.each do |candidate|
   next unless repeating?(candidate)
   next unless increasing?(candidate)
   possibles += 1 
 end
 
-puts "Found #{possibles} possible passwords"
+puts "Found #{possibles} possible passwords for part 1 rules"
+
+possibles = 0
+input.each do |candidate|
+  next unless repeating_but_not_too_much?(candidate)
+  next unless increasing?(candidate)
+  possibles += 1 
+end
+
+puts "Found #{possibles} possible passwords for part 2 rules"
