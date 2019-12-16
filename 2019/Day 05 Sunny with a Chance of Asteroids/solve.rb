64be1146283e9747ee5e281d7799674c395c372e
John Wulff <john@johnwulff.com>
2019-12-16 13:28:58 -0800

    Refactor intcode computer to an object

--- 2019/Day 05 Sunny with a Chance of Asteroids/solve.rb
+++ 2019/Day 05 Sunny with a Chance of Asteroids/solve.rb
@@ -1,31 +1,53 @@
 #!/usr/bin/env ruby
 
-def run(codes)
-  i = 0
-  loop do
-    case codes[i]
-    when 1 # Addition
-      codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]]
-      i += 4
-    when 2 # Multiplication
-      codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]]
-      i += 4
-    when 3 # Input
-      print "Input: "
-      input = gets.strip.to_i
-      codes[codes[i + 1]] = input
-      i += 2
-    when 4 # Output
-      puts "Output: #{codes[codes[i + 1]]}"
-      i += 2
-    when 99 # Halt
-      break
-    else
-      puts "Unknown opscode #{codes[i]} in position #{i}. Halting."
-      break
+class IntcodeComputer
+  attr_accessor :codes, :i
+
+  def initialize(codes)
+    self.codes = codes
+    self.i = 0
+  end
+
+  def run!
+    loop do
+      case opcode
+      when  1; add;
+      when  2; multiply;
+      when  3; input;
+      when  4; output;
+      when 99; break;
+      else
+        puts "Unknown opscode #{opcode} in position #{i}. Halting."
+        break
+      end
     end
   end
-  codes
+
+  def opcode
+    codes[i] % 100
+  end
+
+  def add
+    codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]]
+    self.i += 4
+  end
+
+  def multiply
+    codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]] 
+    self.i += 4
+  end
+
+  def input
+    print "Input: "
+    input = gets.strip.to_i
+    codes[codes[i + 1]] = input
+    self.i += 2
+  end
+
+  def output
+    puts "Output: #{codes[codes[i + 1]]}"
+    self.i += 2
+  end
 end
 
 # Run tests
@@ -37,7 +59,9 @@ puts "Running Self Diagnostic"
   [[1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99]], # Example from Day 2
   [[3,0,4,0,99], [5,0,4,0,99]] # Example for input/output, assumes input of 5
 ].each do |test|
-  result = run test[0]
+  ic = IntcodeComputer.new test[0]
+  ic.run!
+  result = ic.codes
   if result == test[1]
     puts "GO"
   else
